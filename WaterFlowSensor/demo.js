f877be55ea01d14815f0a4a65ea69e8acf009d07
Tyler Mizuyabu <tyler@monanetworks.com>
2017-12-06 15:43:46 -0500

    tweaked demo

--- WaterFlowSensor/demo.js
+++ WaterFlowSensor/demo.js
@@ -12,6 +12,9 @@
 
 const WaterFlowSensor = require('./index');
 const GPIO = require('pigpio').Gpio;
+const EventEmitter = require('events');
+
+const emitter = new EventEmitter();
 
 const sensor = WaterFlowSensor(4);
 
@@ -24,31 +27,42 @@ let lastTime = process.hrtime();
 let lastFlowRate = 0;
 let pumpOn = false;
 
+emitter.on('data', async (data) => {
+  const diff = process.hrtime(lastTime);
+  const flowRate = lastFlowRate;
+
+  lastFlowRate = data; //Sets this as early as possible
+  lastTime = process.hrtime(); //sets this as early as possible
+
+
+  console.log('time diff: ',diff[1] / 1000000000 + diff[0]);
+  console.log('flowRate: ',flowRate * 1000 / 60);
+
+  poured += ( flowRate * 1000 / 60) * (diff[1] / 1000000000 + diff[0]);
+  console.log(poured);
+
+  if (poured >= 250 ) {
+    en1.digitalWrite(0);
+    console.log(`Poured: ${poured}`);
+    process.exit(0);
+  }
+  
+}); //When new flow is read do this
+
 setInterval(
   async () => {
-    const diff = process.hrtime(lastTime);
-    lastTime = process.hrtime();
     if (sensor) {
       if (!pumpOn) {
         en2.digitalWrite(0);
         en1.digitalWrite(1);
         pumpOn=true;
       }
-      console.log('time diff: ',diff[1] / 1000000000 + diff[0]);
-      console.log('flowRate: ',lastFlowRate * 1000 / 60);
       poured += ( lastFlowRate * 1000 / 60) * (diff[1] / 1000000000 + diff[0]);
-      console.log(poured);
-
-      if (poured >= 250 ) {
-        en1.digitalWrite(0);
-        console.log(`Poured: ${poured}`);
-        process.exit(0);
-      }
 
       const flowRate = await sensor.getValue(); //L/min
-      lastFlowRate = flowRate;
-
+      emitter.emit('data', flowRate);
     }
   },
   1100
 );
+
