df18709b8ed49f7970eee90a9e63dd4aa459bd82
Tyler Mizuyabu <tyler@monanetworks.com>
2017-12-14 11:09:38 -0500

    updated git ignore

--- Amazon Api/index.js
+++
@@ -1,8 +0,0 @@
-const Koa = require('koa');
-const http = require('http');
-const path = require('path');
-const app = new Koa();
-
-const httpServer = http.createServer(app.callback()).listen(8080);
-
-app.use(require('koa-static')(path.join(__dirname, '../public')));
